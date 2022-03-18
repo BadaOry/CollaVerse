d138727be4014e2fb8054e9f6e88b260e43a982c
bada.ory <bada.ory@gmail.com>
2022-03-18 12:35:15 +0900

    0318_메인 구현중 : 공지사항 & 프로모션 연결 필요_이수정

--- CollaVerse/src/main/java/com/collaverse/mvc/HomeController.java
+++ CollaVerse/src/main/java/com/collaverse/mvc/HomeController.java
@@ -2,14 +2,21 @@ package com.collaverse.mvc;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.collaverse.mvc.collections.model.dao.CollectionsMapper;
+import com.collaverse.mvc.collections.model.vo.Collections;
 
 /**
  * Handles requests for the application home page.
@@ -22,6 +29,9 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	@Autowired
+	private CollectionsMapper collectionsMapper;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 //		logger.info("Welcome home! The client locale is {}.", locale);
@@ -33,6 +43,17 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		
+		// 3. 컬렉션 랜덤 추천
+		
+		List<Collections> randomUserList = null;
+		
+		randomUserList = collectionsMapper.getRandomUserList();
+		
+		System.out.println("[HomeController] 받아온 랜덤 리스트 출력 : "+ randomUserList.toString());
+		
+		model.addAttribute("randomUserList", randomUserList);
 		
 		return "home";
 	}
